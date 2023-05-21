b6c5da306f55adb9a68dd12fe507e2d9143fed30
Quentin Pradet <quentin.pradet@gmail.com>
2023-05-21 15:27:56 +0400

    Print OpenSSL version

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+python -c 'import ssl; print(ssl.OPENSSL_VERSION)'
+
 # https://blog.ionelmc.ro/2015/04/14/tox-tricks-and-patterns/#when-it-inevitably-leads-to-shell-scripts
 # If you are getting an INVOCATION ERROR for this script then there is 
 # a good chance you are running on Windows.
